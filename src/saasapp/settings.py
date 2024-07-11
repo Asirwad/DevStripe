cb3a99fb6ceb35a021f0ed7ee7bf9f5179c82f0e
Asirwad <asirwadsali@gmail.com>
2024-07-12 00:11:58 +0530

    added .env

--- src/saasapp/settings.py
+++ src/saasapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&xm*7tjw#u(t_kp5d1tsmbl4pvjbzl)wz4z0*quhi3zzrp^sf'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == 'true'
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == 'true'
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
