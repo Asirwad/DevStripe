6ec709c751843f4c90ee2478022c30ed5eadfd0a
Asirwad <asirwadsali@gmail.com>
2024-08-03 21:55:10 +0530

    add cards

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from subscriptions.models import UserSubscription
 
 # Create your views here.
 @login_required
 def dashboard_view(request):
-    return render(request, "dashboard/main.html", {})
+    user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
+    return render(request, "dashboard/main.html", {
+        "subscription": user_sub_obj
+    })
